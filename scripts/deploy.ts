b0815f1ca3e817b6ff32786a67150a31a2b42116
SauravKanchan <sauravnk30@gmail.com>
2021-04-18 20:03:51 +0530

    build(final): modify deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,13 +4,26 @@ import { Contract, ContractFactory } from "ethers";
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import { FlashMinter__factory, FlashWETH__factory } from "../typechain"
 
 async function main(): Promise<void> {
-  // Hardhat always runs the compile task when running scripts through it.
-  // If this runs in a standalone fashion you may want to call compile manually
-  // to make sure everything is compiled
-  // await run("compile");
-  // We get the contract to deploy
+  const FlashWETH = (await ethers.getContractFactory("FlashWETH")) as FlashWETH__factory;
+  const flashWeth = await FlashWETH.deploy();
+
+  console.log(`Flash Wrapped ether deployed to: ${flashWeth.address}`);
+
+  const FlashMinter = (await ethers.getContractFactory("FlashMinter")) as FlashMinter__factory;
+
+  const flashMinter = await FlashMinter.deploy(flashWeth.address)
+  console.log(`Flash Minter deployed to: ${flashMinter.address}`);
+
+  const tx = await flashWeth.flashMint(flashMinter.address, ethers.utils.parseEther("100"), [])
+
+  await tx.wait();
+
+  const amountMinted = await flashMinter.totalBorrowed();
+ 
+  console.log("Flash Minted account", amountMinted.toNumber())
 }
 
 // We recommend this pattern to be able to use async/await everywhere
